dad1dfcc95e58c30faef512ffdac38df3310a02b
navnitamandal <navnimandal@gmail.com>
2024-05-22 10:17:34 +0530

    how to upload a file using sendKeys

---
+++ src/test/java/org/example/newTest.java
@@ -0,0 +1,19 @@
+package org.example;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class newTest {
+    public static void main(String[] args) {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+        driver.get("https://www.tutorialspoint.com/selenium/practice/upload-download.php");
+        driver.findElement(By.xpath("//input[@id='uploadFile']")).sendKeys("C:\\Users\\Windows11\\Documents\\study\\notes.txt");
+    }
+}
